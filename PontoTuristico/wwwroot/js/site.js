9ee2186c776690f90417cd79f3a2392841fdac9c
ErikFroza <erikfroza@hotmail.com>
2024-05-17 12:42:29 -0300

    Finalização do projeto. Foi adicionado a API ViaCEP e o plugin JqueryMask.

--- PontoTuristico/wwwroot/js/site.js
+++ PontoTuristico/wwwroot/js/site.js
@@ -35,7 +35,62 @@ $(document).ready(function () {
         }
     });
 
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#referencia").val("");
+        $("#cidade").val("");
+        $("#estado").val("");
+    }
+
+    //Quando o campo cep perde o foco.
+    $("#cep").blur(function () {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if (validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#referencia").val("...");
+                $("#cidade").val("...");
+                $("#estado").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#referencia").val(dados.logradouro);
+                        $("#cidade").val(dados.localidade);
+                        $("#estado").val(dados.uf);
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
+
 });
+
+$('#cep').mask('00000-000');
 
 $('.close-alert').click(function () {
     $(".alert").hide('hide');
